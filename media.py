3705d48c7ed965d81077868c4070c6ea68f5eceb
Nathcb <108531915+Nathcb@users.noreply.github.com>
2022-07-27 18:52:13 -0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,10 @@
+PrimeiraNota = input ("Digite a primeira nota: ")
+X = int (PrimeiraNota)
+SegundaNota = input ("Digite a segunda nota: ")
+Y = int (SegundaNota)
+TerceiraNota = input ("Digite a terceira nota: ")
+Z = int (TerceiraNota)
+QuartaNota = input ("Digite a quarta nota: ")
+A = int (QuartaNota)
+Media = (X + Y + Z + A) / 4
+print ("A média aritmética é", Media)
---
+++ perimetro e area de um quadrado.py
@@ -0,0 +1,5 @@
+x = input ("Digite o valor correspondente ao lado de um quadrado: ")
+y = int(x)
+perimetro = y * 4
+area = y * y
+print ("perímetro:", perimetro, "- área:",area)
